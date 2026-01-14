4406c4f5c83297b0b083c9446ed0b3361cf9b20d
mulatta <67085791+mulatta@users.noreply.github.com>
2026-01-14 23:58:34 +0900

    initial commit

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "blog.mulatta.io";
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+
+      imports = [
+        ./nix/shell.nix
+        ./nix/formatter.nix
+        ./nix/packages.nix
+      ];
+
+      perSystem = {
+        lib,
+        self',
+        system,
+        ...
+      }: {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+
+        checks = let
+          packages = lib.mapAttrs' (n: lib.nameValuePair "package-${n}") self'.packages;
+          devShells = lib.mapAttrs' (n: lib.nameValuePair "devShell-${n}") self'.devShells;
+        in
+          {inherit (self') formatter;} // packages // devShells;
+      };
+    };
+
+  inputs = {
+    # keep-sorted start
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    # keep-sorted end
+  };
+}
